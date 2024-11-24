38432480104c49c297151db2ce3b5a771bb632e9
doomangang <sp0943@cau.ac.kr>
2024-11-24 18:31:16 +0900

    impl:done

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -74,3 +74,110 @@ std::ostream& operator <<(std::ostream &out, const Fixed &fixed)
 	out << fixed.toFloat();
 	return out;
 }
+
+bool                Fixed::operator>(const Fixed &other) const
+{
+	return (this->getRawBits() > other.getRawBits());
+}
+
+bool                Fixed::operator<(const Fixed &other) const
+{
+	return (this->getRawBits() < other.getRawBits());
+}
+
+bool                Fixed::operator>=(const Fixed &other) const
+{
+	return (this->getRawBits() >= other.getRawBits());
+}
+
+bool                Fixed::operator<=(const Fixed &other) const
+{
+	return (this->getRawBits() <= other.getRawBits());
+}
+
+bool                Fixed::operator==(const Fixed &other) const
+{
+	return (this->getRawBits() == other.getRawBits());
+}
+
+bool                Fixed::operator!=(const Fixed &other) const
+{
+	return (this->getRawBits() != other.getRawBits());
+}
+
+Fixed              Fixed::operator+(const Fixed &other) const
+{
+	Fixed   result(this->toFloat() + other.toFloat());
+	return (result);
+}
+
+Fixed              Fixed::operator-(const Fixed &other) const
+{
+	Fixed   result(this->toFloat() + other.toFloat());
+	return (result);
+}
+
+Fixed              Fixed::operator*(const Fixed &other) const
+{
+	Fixed   result(this->toFloat() * other.toFloat());
+	return (result);
+}
+
+Fixed              Fixed::operator/(const Fixed &other) const
+{
+	Fixed   result(this->toFloat() / other.toFloat());
+	return (result);
+}
+
+Fixed&              Fixed::operator++(void)
+{
+	_value++;
+	return (*this);
+}
+
+const Fixed         Fixed::operator++(int)
+{
+	const Fixed copy = Fixed(*this);
+	_value++;
+	return (copy);
+}
+
+Fixed&              Fixed::operator--(void)
+{
+	_value--;
+	return (*this);
+}
+
+const Fixed         Fixed::operator--(int)
+{
+	const Fixed copy = Fixed(*this);
+	_value--;
+	return (copy);
+}
+
+Fixed&       Fixed::min(Fixed& ref1, Fixed& ref2)
+{
+	if (ref1 < ref2)
+		return ref1;
+	return ref2;
+}
+const Fixed& Fixed::min(Fixed const& ref1, Fixed const& ref2)
+{
+	if (ref1 < ref2)
+		return ref1;
+	return ref2;
+}
+
+Fixed&       Fixed::max(Fixed& ref1, Fixed& ref2)
+{
+	if (ref1 > ref2)
+		return ref1;
+	return ref2;
+}
+
+const Fixed& Fixed::max(Fixed const& ref1, Fixed const& ref2)
+{
+	if (ref1 > ref2)
+		return ref1;
+	return ref2;
+}
--- ex02/Fixed.hpp
+++ ex02/Fixed.hpp
@@ -30,7 +30,24 @@ public:
 	float               toFloat( void ) const;
 	int                 toInt( void ) const;
 	Fixed&              operator=(const Fixed &other);
-
+	bool                operator>(const Fixed &other) const;
+	bool                operator<(const Fixed &other) const;
+	bool                operator>=(const Fixed &other) const;
+	bool                operator<=(const Fixed &other) const;
+	bool                operator==(const Fixed &other) const;
+	bool                operator!=(const Fixed &other) const;
+	Fixed               operator+(const Fixed &other) const;
+	Fixed               operator-(const Fixed &other) const;
+	Fixed               operator*(const Fixed &other) const;
+	Fixed               operator/(const Fixed &other) const;
+	Fixed&              operator++(void);
+	const Fixed         operator++(int);
+	Fixed&              operator--(void);
+	const Fixed         operator--(int);
+	static Fixed&       min(Fixed& ref1, Fixed& ref2);
+	static const Fixed& min(Fixed const& ref1, Fixed const& ref2);
+	static Fixed&       max(Fixed& ref1, Fixed& ref2);
+	static const Fixed& max(Fixed const& ref1, Fixed const& ref2);
 };
 
 std::ostream& operator  <<(std::ostream &out, const Fixed &fixed);
